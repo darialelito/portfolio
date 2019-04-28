32d5ccf68f2ac19512b0ea7d24463ef8eb1fe836
Daria Lelito <darialelito@gmail.com>
2019-04-28 19:10:10 +0200

    start with desktop view

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const aboutBtn = document.querySelector(".aboutButton");
 const aboutSection = document.querySelector(".section_about");
+const footerArrowShow = document.querySelector(".footer-arrow")
 
 aboutBtn.addEventListener('click', function () {
     aboutSection.classList.toggle('on');
+    footerArrowShow.classList.add('on');
 })
 
 const portfolioBtn = document.querySelector(".portfolioButton");
@@ -10,4 +12,17 @@ const portfolioSection = document.querySelector(".section_portfolio");
 
 portfolioBtn.addEventListener('click', function () {
     portfolioSection.classList.toggle('on');
+    footerArrowShow.classList.add('on');
+})
+
+const contactBtn = document.querySelector(".contactButton");
+const contactSection = document.querySelector(".section_contact");
+
+contactBtn.addEventListener('click', function () {
+    contactSection.classList.toggle('on');
+    footerArrowShow.classList.add('on');
+})
+
+footerArrowShow.addEventListener('click', function () {
+    window.scrollTo(0, 0);
 })
